0460eabf1011eb9386d3cfdbfef86e891d51a237
Nestor Schneemann <nestor.schneemann@gmail.com>
2023-01-02 21:18:01 -0500

    Modificamos la funcion de diagrama de Torta para que guarde una imagen con el nombre dle continente de interes

--- poblacion_mundial.py
+++ poblacion_mundial.py
@@ -4,8 +4,10 @@ import charts
 
 def run():
   data = read_csv.read_csv('./world_population.csv')
+  continent = input('Escribe el nombre del continente de tu interes ')
+  data = list(filter(lambda item: item['Continent'] == continent, data))
   countries, percentage = utils.percentage_by_country_v2(data)
-  charts.generate_pie_chart(countries, percentage)
+  charts.generate_pie_chart(continent, countries, percentage)
   
 if __name__ == '__main__':
   run()
